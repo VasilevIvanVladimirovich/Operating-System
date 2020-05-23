242b975db853d5cc1b2f7740d4f4967533f224b5
Иван Васильев <65816571+VasilevIvanVladimirovich@users.noreply.github.com>
2020-05-23 16:24:39 +0300

    Create receiver.c

---
+++ Lab03/receiver.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <sys/shm.h>
+#include <stdlib.h>
+
+int comp(const int *i,const int *j);
+
+int main(int argv, char *argc[])
+{
+	if(argv<=1){
+		printf("not enough params\n");
+		return -1;
+	}
+	char *paramStr=argc[1];
+	int memId=atoi(paramStr);
+	
+	if(memId==0)
+	{
+		printf("incorrect parameter string:%s\n",paramStr);
+		return -2;
+	}
+	printf("receiving the memory data: shmid = %i\n", memId);
+	
+	int *mem=(int *)shmat(memId,0,0);
+	if(NULL==mem)
+	{
+		printf("error with shmat\n");
+		return -3;
+	}
+	
+	printf("original array: \n");
+	for (int i=0;i<20;i++) printf(" %i ",mem[i]);
+	printf("\n");
+	printf("Sorted array: \n");
+	qsort(mem,20,sizeof(int),(int(*)(const void *,const void *))comp);
+	for(int i = 0;i<20;i++) printf(" %i ",mem[i]);
+	printf("\n");
+	return 0;
+}
+
+int comp(const int *i,const int *j)
+{
+	return *i-*j;
+}
